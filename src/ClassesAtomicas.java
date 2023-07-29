2d954b4f21c2f401a3bcfa273d70a589a5ef469e
Elias Moura <elias.smourayt@gmail.com>
2023-07-29 11:30:40 -0300

    Classes como AtomicInt, AtomicBolean, AtomicLong para uso com threads que asseguram que duas operações como increntar e pegar serão realizadas em thread safe.

---
+++ src/ClassesAtomicas.java
@@ -0,0 +1,33 @@
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ClassesAtomicas {
+//    static AtomicInteger i = new AtomicInteger(-1);
+//    static AtomicBoolean b = new AtomicBoolean(false);
+    static AtomicReference<Object> r = new AtomicReference<>(new Object());
+    public static void main(String[] args) throws InterruptedException {
+        MeuRunnable runnable = new MeuRunnable();
+
+        Thread t0 = new Thread(runnable);
+        Thread t1 = new Thread(runnable);
+        Thread t2 = new Thread(runnable);
+
+        t0.start();
+        t1.start();
+        t2.start();
+        Thread.sleep(500);
+    }
+
+    public static class MeuRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            String name = Thread.currentThread().getName();
+//            System.out.println(name + ":" + i.incrementAndGet());
+//            System.out.println(name + ":" + b.compareAndExchange(false, true));
+            System.out.println(name + ":" + r.getAndSet(new Object()));
+        }
+    }
+}
